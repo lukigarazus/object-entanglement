9905825857aa17ea58c5900d9aa4b39b83d00e32
Łukasz <lu.karczewski@gmail.com>
2020-06-15 11:07:25 +0000

    Enable disentanglement

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-export const ENTANGLED = Symbol("coupled");
+var ENTANGLED = Symbol("entangle");
+var DISENTANGLE = Symbol("disentangle");
 
-export const entangle = (obj1, obj2) => {
+module.exports.ENTANGLED   = ENTANGLED;
+module.exports.DISENTANGLE = DISENTANGLE
+
+module.exports.entangle = (obj1, obj2) => {
   const clone = { ...obj2 };
   Object.keys(obj1).forEach(key => {
     if (typeof obj1[key] !== "number") {
-      throw new Error("Can couple only numbers");
+      console.log("Can entangle only numeric keys");
+      return
     }
     obj1[key] += obj2[key];
     delete obj2[key];
@@ -19,9 +24,23 @@ export const entangle = (obj1, obj2) => {
       configurable: true
     });
   });
-  Object.defineProperty(obj2, COUPLED, {
+  Object.defineProperty(obj2, ENTANGLED, {
     value: { target: obj1, original: clone },
-    configurable: false,
+    configurable: true,
+    enumerable: false
+  });
+  Object.defineProperty(obj2, DISENTANGLE, {
+    get() {
+      delete this[DISENTANGLE];
+      Object.keys(this).forEach(key => {
+        delete this[key]
+        this[key] = this[ENTANGLED].original;
+      })
+      delete this[ENTANGLED];
+    },
+    configurable: true,
     enumerable: false
   });
 };
+
+
